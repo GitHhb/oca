17b5cffcb437243fe9dc32db25107469934114ba
GitHhb <github@mittere.net>
2016-01-10 05:09:52 +0100

    First setup

--- src/HogerLager.java
+++ src/HogerLager.java
@@ -2,9 +2,11 @@
  * Created by leandro on 9-1-2016.
  */
 public class HogerLager {
-    public static void main(String[] arg(){
+    public static void main(String[] arg){
 
     }
 }
 
-class
+
+enum KaartKleur {Schoppen, Ruiten, Harten, Klaver}
+//enum KaartWaarde {1, 2, 3, 4, 5, 6, 7, 8, 9, Boer, Vrouw, Heer, Aas}
--- src/Telefoonboek.java
+++ src/Telefoonboek.java
@@ -2,22 +2,27 @@ import java.util.ArrayList;
 
 public class Telefoonboek {
     public static void main(String[] args){
-        ArrayList<PersonData> persons = new ArrayList<>();
+        //ArrayList<PersonData> persons = new ArrayList<>();
+        PersonData persons = new PersonData();
         persons.add(new PersonData("Jan", "Jansen", "040123", false));
         persons.add(new PersonData("Piet", "Pieters", "040124", true));
 
         //System.out.println(persons.get(0));
-        PersonData.printAll(persons);
+        persons.printAll();
 
+        persons.searchFirstName("Piet");
     }
 }
 
-class PersonData {
+class PersonData extends ArrayList<PersonData> {
     // Kenmerken van een persoon
     String firstName;
     String lastName;
     String phoneNr;
     Boolean phoneNrSecret;
+
+    // Constructors
+    PersonData() {}
 
     PersonData(String firstName, String lastName, String phoneNr, Boolean phoneNrSecret){
         this.firstName = firstName;
@@ -27,13 +32,23 @@ class PersonData {
         System.out.println("Added: " + firstName);
     }
 
+    // methods
     public String toString(){
         return firstName + " " + lastName + ", phone: " + (phoneNrSecret? "*******" : phoneNr);
     }
 
-    static public void printAll(ArrayList<PersonData> persons){
-        for (PersonData p: persons)
-            System.out.println(p);
+    public void printAll(){
+        for (PersonData p: this)
+            printPerson(p);
+    }
+
+    public void printPerson(PersonData p){
+        System.out.println(p);
+    }
+
+    public void searchFirstName (String firstName){
+        for (PersonData p: this)
+            if (p.firstName.equals(firstName)) printPerson(p);
     }
 
 }
